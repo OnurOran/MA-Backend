2412b643d24b0a6f80bde1dd7fd2ec1376e18238
Onur Oran <onur.oran@metro.istanbul>
2026-01-23 06:32:08 +0000

    matrix migration file added

---
+++ src/SurveyBackend.Infrastructure/Scripts/Migration4_AddMatrixQuestionType.sql
@@ -0,0 +1,85 @@
+﻿BEGIN TRANSACTION;
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260122092458_AddMatrixQuestionType'
+)
+BEGIN
+    ALTER TABLE [Question] ADD [MatrixExplanationLabel] nvarchar(200) NULL;
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260122092458_AddMatrixQuestionType'
+)
+BEGIN
+    ALTER TABLE [Question] ADD [MatrixScale1Label] nvarchar(100) NULL;
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260122092458_AddMatrixQuestionType'
+)
+BEGIN
+    ALTER TABLE [Question] ADD [MatrixScale2Label] nvarchar(100) NULL;
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260122092458_AddMatrixQuestionType'
+)
+BEGIN
+    ALTER TABLE [Question] ADD [MatrixScale3Label] nvarchar(100) NULL;
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260122092458_AddMatrixQuestionType'
+)
+BEGIN
+    ALTER TABLE [Question] ADD [MatrixScale4Label] nvarchar(100) NULL;
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260122092458_AddMatrixQuestionType'
+)
+BEGIN
+    ALTER TABLE [Question] ADD [MatrixScale5Label] nvarchar(100) NULL;
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260122092458_AddMatrixQuestionType'
+)
+BEGIN
+    ALTER TABLE [Question] ADD [MatrixShowExplanation] bit NOT NULL DEFAULT CAST(0 AS bit);
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260122092458_AddMatrixQuestionType'
+)
+BEGIN
+    ALTER TABLE [AnswerOption] ADD [Explanation] nvarchar(500) NULL;
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260122092458_AddMatrixQuestionType'
+)
+BEGIN
+    ALTER TABLE [AnswerOption] ADD [ScaleValue] int NULL;
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260122092458_AddMatrixQuestionType'
+)
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20260122092458_AddMatrixQuestionType', N'10.0.0');
+END;
+
+COMMIT;
+GO
+
